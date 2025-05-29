073623a7369c87b64a97e31bfda444947b5361c9
danapalania <dana.p.alania@ciit.edu.ph>
2025-05-29 16:20:41 +0800

    update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('registration_date');
+            $table->timestamp('last_login_date')->nullable();
+            $table->dropTimestamps();
+
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
---
+++ database/migrations/2025_05_29_015159_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('comment_content')->comment('Contents of the comment');
+            $table->timestamp('comment_date')->comment('Date the comment was made');
+            $table->string('reviwer_name')->comment('name of reviwer');
+            $table->string('reviwer_email')->comment('email of reviewer');
+            $table->boolean('is_hidden')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
