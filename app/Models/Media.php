13f40c7bd8f2d4b4975f22545d41ebc8c69bd2a3
danapalania <dana.p.alania@ciit.edu.ph>
2025-06-11 15:07:30 +0800

    defined relationships

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Media extends Model
 {
-    //
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
-    //
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_role');
+    }
 }
